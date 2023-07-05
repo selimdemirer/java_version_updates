112f2eae52a418957367bad249d5129bbfc59b81
selimdemirer <selimmdemirerr@gmail.com>
2023-07-05 10:49:43 +0300

    m2-enum module updated

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -2,6 +2,6 @@ package com.cydeo;
 
 public enum Currency {
 
-    PENNY,NICKLE,DIME,QUARTER
+    PENNY,NICKLE,DIME,QUARTER // Everything you put in the enum is a "Constant Object of This Enum Class"
 
 }
