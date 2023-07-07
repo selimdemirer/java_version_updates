fcfb2ff236e0b856f9f997596f13541bd1f6947e
selimdemirer <selimmdemirerr@gmail.com>
2023-07-07 10:52:16 +0300

    Gneric Swap Task Completed

---
+++ m1-generics/src/main/java/com/cydeo/tasks/SwapValues.java
@@ -0,0 +1,28 @@
+package com.cydeo.tasks;
+
+import java.util.Arrays;
+
+public class SwapValues {
+
+    public static void main(String[] args) {
+
+        Integer[] a = {1,2,3,4};
+        swap(a,0,2);
+        String[] b = {"Esra","Zehra","Zeynep"};
+        swap(b,0,1);
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+
+    }
+
+    private static <T> void swap(T[] a, int i, int j){
+
+              T temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+
+    }
+
+
+
+}
