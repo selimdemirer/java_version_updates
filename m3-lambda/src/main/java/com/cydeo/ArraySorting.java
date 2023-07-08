89aea868b32c25064d4f1667f00ee5682e7cef68
selimdemirer <selimmdemirerr@gmail.com>
2023-07-08 20:55:07 +0300

    Module-3 Lambda created

--- m3-lambda/src/main/java/com/cydeo/ArraySorting.java
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -5,12 +5,17 @@ public class ArraySorting { //QuickSort, BubbleSort
     public static void main(String[] args) {
 
         ArraySorting as = new ArraySorting();
-        as.sort();
+
+        QuickSort qs = new QuickSort();
+        BubbleSort bs = new BubbleSort();
+
+        as.sort(qs);
+        as.sort(bs);
 
     }
 
-    private void sort(){
-        System.out.println("Quick Sorting Array");
+    private void sort(Sorting sorting){
+        sorting.sort();
     }
 
 
