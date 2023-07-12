288e57af7a44c7824249f207972a2147a6331102
selimdemirer <selimmdemirerr@gmail.com>
2023-07-12 22:09:59 +0300

    comparator.comparing completed

--- m5-comparator_interface/src/main/java/com/cydeo/appleTask/AppleTest.java
+++ m5-comparator_interface/src/main/java/com/cydeo/appleTask/AppleTest.java
@@ -1,7 +1,10 @@
 package com.cydeo.appleTask;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class AppleTest {
 
@@ -12,6 +15,20 @@ public class AppleTest {
         inventory.add(new Apple(100, Color.RED));
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
+
+        //Ascending
+        inventory.sort(comparing(Apple::getWeight));
+        System.out.println(inventory);
+
+        //Descending
+        inventory.sort(comparing(Apple::getWeight).reversed());
+        System.out.println(inventory);
+
+        //Chaining
+        inventory
+                .sort(comparing(Apple::getWeight)
+                        .reversed().thenComparing(Apple::getColor));
+
 
     }
 
