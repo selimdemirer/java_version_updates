643d7952174785c37ca0a1d6ed9f4deb429ab28d
selimdemirer <selimmdemirerr@gmail.com>
2023-07-12 14:05:53 +0300

    My Class completed

--- m4-double_colon_operators/src/main/java/com/cydeo/Main.java
+++ m4-double_colon_operators/src/main/java/com/cydeo/Main.java
@@ -1,6 +1,8 @@
 package com.cydeo;
 
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Main {
@@ -29,6 +31,13 @@ public class Main {
 
         BiFunction<String,Integer,String> func1 = (str,i) -> str.substring(i);
         BiFunction<String,Integer,String> func2 = String::substring;
+
+        //Reference to an Instance Method
+        Function <Integer,Double> b = new MyClass()::method; // we can use the double colon op after "obj"
+        BiFunction<MyClass,Integer,Double> b1 = MyClass::method; // we can use the double colon op after class name for same instance method. Because "obj" belongs to this class.
+
+        Consumer<Integer> display1 = i -> System.out.println(i);
+        Consumer<Integer> display2 = System.out::println;
 
 
 
---
+++ m4-double_colon_operators/src/main/java/com/cydeo/MyClass.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+public class MyClass {
+
+    public double method(int x){
+        return x*2.5;
+    }
+
+}
