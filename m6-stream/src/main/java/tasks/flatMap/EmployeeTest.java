a2446e2328181e2d9af6e57155426e2f9689fe97
selimdemirer <selimmdemirerr@gmail.com>
2023-07-14 13:12:27 +0300

    flatMap task completed

---
+++ m6-stream/src/main/java/tasks/flatMap/EmployeeTest.java
@@ -0,0 +1,36 @@
+package tasks.flatMap;
+
+import java.util.List;
+
+public class EmployeeTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Print all emails");
+        EmployeeData.readAll() // this is already converted to stream because of "Stream.of()" ! No need to write ".stream()"!         //  .map(employee -> employee.getEmpEmail())
+                .map(Employee::getEmpEmail)
+                .forEach(System.out::println);
+
+        System.out.println("Print all phone numbers");
+        EmployeeData.readAll()
+                .flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+
+        System.out.println("or with Double Colon Operator");
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
